0ebaf6309e3f9884588381b101264c87332f74bc
Rogerio Pereira <hi@rogeriopereira.dev>
2025-08-26 20:52:16 -0400

    Enriquecendo documentos

---
+++ 05-data-loading-e-rag/03-ingestion-pgvector.py
@@ -0,0 +1,59 @@
+import os 
+from pathlib import Path
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
+
+load_dotenv()
+for k in ("GOOGLE_API_KEY", "PGVECTOR_URL", "PGVECTOR_COLLECTION"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
+current_dir = Path(__file__).parent
+pdf_path = current_dir / "gpt5.pdf"
+C
+docs = PyPDFLoader(str(pdf_path)).load()
+
+splits = RecursiveCharacterTextSplitter(
+                chunk_size=1000, 
+                chunk_overlap=150,
+                add_start_index=False
+            ).split_documents(docs)
+
+if not splits:
+    raise SystemExit(0)
+
+# Runs a loop over all splits, creating a Document at each iteration
+#   Each Document will have:
+#       page_content from the documents being iterated inside the splits
+#       metadata (with the metadata key and value), e.g., title, source, page
+#           Only if the value is not empty
+enriched = [
+    Document(
+        page_content=d.page_content,
+        metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+    )
+
+    for d in splits
+]
+# readable form
+#
+# enriched = []
+# for d in splits:
+#     page_content = d.page_content
+#
+#     meta = {}
+#     for k, v in d.metadata.items():
+#         if v not in ("", None):
+#             meta[k] = v
+#
+#     doc = Document(
+#         page_content=page_content,
+#         metadata=meta
+#     )
+#
+#     enriched.append(doc)
